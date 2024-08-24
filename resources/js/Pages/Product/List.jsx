93bfb51349196bb3248e1c9722ea64940683a803
Andrei <serban.c.andrei@gmail.com>
2024-08-25 01:34:37 +0300

    Rezolvare filtre, search bar, paginare. Tema 4

--- resources/js/Pages/Product/List.jsx
+++ resources/js/Pages/Product/List.jsx
@@ -56,55 +56,61 @@ export default function List({ product, categories, selectedCategory }) {
                             <FontAwesomeIcon icon={faPlus} /> Add new product
                         </Link>
                     </div>
+                    {product && product.data.length > 0 ? (
+                        <>
+                            <div className="mt-6">
+                                <div className='grid grid-cols-7'>
+                                    <div className='font-bold mb-3'>ID</div>
+                                    <div className='font-bold mb-3'>Name</div>
+                                    <div className='font-bold mb-3'>Description</div>
+                                    <div className='font-bold mb-3'>Category</div>
+                                    <div className='font-bold mb-3'>Price</div>
+                                    <div className='font-bold mb-3'>Quantity</div>
+                                    <div className='font-bold mb-3'>Actions</div>
 
-                    <div className="mt-6">
-                        <div className='grid grid-cols-7'>
-                            <div className='font-bold mb-3'>ID</div>
-                            <div className='font-bold mb-3'>Name</div>
-                            <div className='font-bold mb-3'>Description</div>
-                            <div className='font-bold mb-3'>Category</div>
-                            <div className='font-bold mb-3'>Price</div>
-                            <div className='font-bold mb-3'>Quantity</div>
-                            <div className='font-bold mb-3'>Actions</div>
+                                    {product.data.map((product, index) => {
+                                        return <Fragment key={index}>
+                                            <div className='mb-2'>{product.id}</div>
+                                            <div className='mb-2'>{product.name}</div>
+                                            <div className='mb-2'>{product.description}</div>
+                                            <div className='mb-2'>{product.category.name}</div>
+                                            <div className='mb-2'>{product.price}</div>
+                                            <div className='mb-2'>{product.quantity}</div>
+                                            <div className={'mb-2 flex items-center'}>
+                                                <Link href={route('product.update', [product.id])}>
+                                                    <FontAwesomeIcon icon={faPencil} className={'text-blue-600'} />
+                                                </Link>
 
-                            {product.data.map((product, index) => {
-                                return <Fragment key={index}>
-                                    <div className='mb-2'>{product.id}</div>
-                                    <div className='mb-2'>{product.name}</div>
-                                    <div className='mb-2'>{product.description}</div>
-                                    <div className='mb-2'>{product.category.name}</div>
-                                    <div className='mb-2'>{product.price}</div>
-                                    <div className='mb-2'>{product.quantity}</div>
-                                    <div className={'mb-2 flex items-center'}>
-                                        <Link href={route('product.update', [product.id])}>
-                                            <FontAwesomeIcon icon={faPencil} className={'text-blue-600'} />
-                                        </Link>
-
-                                        {/* Form for deleting the category */}
-                                        <form
-                                            onSubmit={(e) => {
-                                                e.preventDefault();
-                                                handleDeleteProduct(product.id);
-                                            }}
-                                            className={"inline-block ml-2"}
-                                        >
-                                            <button type="submit">
-                                                <FontAwesomeIcon icon={faTrash} className={'text-red-600'} />
-                                            </button>
-                                        </form>
-                                    </div>
-                                </Fragment>
-                            })}
-
-                            <div className={'flex justify-center mt-4 mb-6'}>
-                                {product.links.map((link, key) => (<Fragment key={key}>
-                                    {link.url && !link.active && <Link className={'bg-blue-500 p-2 text-white mr-2'} href={link.url}>
-                                        <span dangerouslySetInnerHTML={{ __html: link.label }} /></Link>}
-                                    {link.url && link.active && <span className={'bg-gray-500 p-2 text-white mr-2'}>{link.label}</span>}
-                                </Fragment>))}
+                                                {/* Form for deleting the category */}
+                                                <form
+                                                    onSubmit={(e) => {
+                                                        e.preventDefault();
+                                                        handleDeleteProduct(product.id);
+                                                    }}
+                                                    className={"inline-block ml-2"}
+                                                >
+                                                    <button type="submit">
+                                                        <FontAwesomeIcon icon={faTrash} className={'text-red-600'} />
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </Fragment>
+                                    })}
+                                    {product.data.length >= 3 && (
+                                        <div className={'flex justify-center mt-4 mb-6'}>
+                                            {product.links.map((link, key) => (<Fragment key={key}>
+                                                {link.url && !link.active && <Link className={'bg-blue-500 p-2 text-white mr-2'} href={link.url}>
+                                                    <span dangerouslySetInnerHTML={{ __html: link.label }} /></Link>}
+                                                {link.url && link.active && <span className={'bg-gray-500 p-2 text-white mr-2'}>{link.label}</span>}
+                                            </Fragment>))}
+                                        </div>
+                                    )}
+                                </div>
                             </div>
-                        </div>
-                    </div>
+                        </>
+                    ) : (
+                        <div><div className='flex flex-row justify-center text-xl font-bold'>Sorry, no products found 😢</div></div>
+                    )}
                 </div>
             </div>
         </AuthenticatedLayout >
